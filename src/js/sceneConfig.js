55524132de1f2b2edc95de7bc28a5cdaf37b1eed
DoctorTone <drtonesoftware@gmail.com>
2019-09-13 14:59:04 +0100

    Initial commit.

---
+++ src/js/sceneConfig.js
@@ -0,0 +1,22 @@
+// General parameters to help with setting up scene
+
+const SceneConfig = {
+    clearColour: 0x5c5f64,
+    ambientLightColour: 0x383838,
+    pointLightColour: 0xffffff,
+    CameraPos: {
+        x: 0,
+        y: 150,
+        z: 300
+    },
+    LookAtPos: {
+        x: 0,
+        y: 0,
+        z: 0
+    },
+    NEAR_PLANE: 0.1,
+    FAR_PLANE: 10000,
+    FOV: 45
+};
+
+export { SceneConfig };
